0496cbd571fb36bd71cc0cc119a911c61a6ddb53
Plotnikov Andrey <shemplo@outlook.com>
2019-10-28 12:25:25 +0300

    Migration to JRE12

--- src/main/java/ru/shemplo/chat/neerc/RunNeercChatClient.java
+++ src/main/java/ru/shemplo/chat/neerc/RunNeercChatClient.java
@@ -2,13 +2,21 @@ package ru.shemplo.chat.neerc;
 
 import javafx.application.Application;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import ru.shemplo.chat.neerc.gfx.ClientAdapter;
 import ru.shemplo.chat.neerc.gfx.WindowManager;
 import ru.shemplo.chat.neerc.network.ConnectionService;
 import ru.shemplo.snowball.annot.Snowflake;
 import ru.shemplo.snowball.annot.processor.Snowball;
 
+@Slf4j
 public class RunNeercChatClient extends Snowball {
+    
+    /*
+     * TODO: change sequence of initializations
+     * Now it's: Snowball.shape () -> Application.launch ()
+     * Project: Application.launch () -> Snowball.shape ()
+     */
     
     public static void main (String... args) { shape (args); }
     
@@ -29,7 +37,7 @@ public class RunNeercChatClient extends Snowball {
             
             while (!window.isInitialized ()) {} // Waiting for initialization of graphics
             connectionService.connect (); /* First auto connection */
-        } catch (Exception es) { es.printStackTrace(); }
+        } catch (Exception | Error es) { log.error (es.getMessage ()); }
     }
     
 }
